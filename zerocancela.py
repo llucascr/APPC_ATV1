9fd934e8d23953a1d1ca991a6c4c490d4eaa7dbb
Fillipe Faria <163869473+FillipeFar1a@users.noreply.github.com>
2024-04-03 11:34:31 -0300

    Add files via upload

--- zerocancela.py
+++ zerocancela.py
@@ -2,15 +2,18 @@ import os
 
 soma = 0
 ult = 0
+pen = 0
 zero = 1
 cons = 0
 desc = 0
 
 os.system('cls')
-
+print("Bem Vindo ao Zero Cancela")
 while 1 == 1:
     num = int (input("Número: "))
     if num > 0:
+        if pen == 0 or pen!=num:
+            pen = ult
         soma += num
         ult = num
         cons+=1
@@ -21,11 +24,13 @@ while 1 == 1:
             soma=soma-ult
             zero+=1
             desc+=1
+            cons= cons-1
+            ult=pen
     elif num == 0 and soma==0:
         print("Nenhum número para desconciderar")
     elif num < 0:
         break
-
+print("Zero Cancela Finalizado")
 print("soma = ", soma)
 print("numeros considerados =", cons)
 print("numeros desconsiderados =", desc)
